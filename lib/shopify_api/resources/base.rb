d370e6e267abdacb2fb7deec6f7fec53dad9e689
serghei <agilestep@yahoo.com>
2020-07-09 15:45:43 +0300

    Safe find method

--- lib/shopify_api/resources/base.rb
+++ lib/shopify_api/resources/base.rb
@@ -73,6 +73,92 @@ module ShopifyAPI
         find :all, *([options] + args)
       end
 
+      #gems/activeresource-5.1.1/lib/active_resource/base.rb
+      # Core method for finding resources. Used similarly to Active Record's +find+ method.
+      #
+      # ==== Arguments
+      # The first argument is considered to be the scope of the query. That is, how many
+      # resources are returned from the request. It can be one of the following.
+      #
+      # * <tt>:one</tt> - Returns a single resource.
+      # * <tt>:first</tt> - Returns the first resource found.
+      # * <tt>:last</tt> - Returns the last resource found.
+      # * <tt>:all</tt> - Returns every resource that matches the request.
+      #
+      # ==== Options
+      #
+      # * <tt>:from</tt> - Sets the path or custom method that resources will be fetched from.
+      # * <tt>:params</tt> - Sets query and \prefix (nested URL) parameters.
+      #
+      # ==== Examples
+      #   Person.find(1)
+      #   # => GET /people/1.json
+      #
+      #   Person.find(:all)
+      #   # => GET /people.json
+      #
+      #   Person.find(:all, :params => { :title => "CEO" })
+      #   # => GET /people.json?title=CEO
+      #
+      #   Person.find(:first, :from => :managers)
+      #   # => GET /people/managers.json
+      #
+      #   Person.find(:last, :from => :managers)
+      #   # => GET /people/managers.json
+      #
+      #   Person.find(:all, :from => "/companies/1/people.json")
+      #   # => GET /companies/1/people.json
+      #
+      #   Person.find(:one, :from => :leader)
+      #   # => GET /people/leader.json
+      #
+      #   Person.find(:all, :from => :developers, :params => { :language => 'ruby' })
+      #   # => GET /people/developers.json?language=ruby
+      #
+      #   Person.find(:one, :from => "/companies/1/manager.json")
+      #   # => GET /companies/1/manager.json
+      #
+      #   StreetAddress.find(1, :params => { :person_id => 1 })
+      #   # => GET /people/1/street_addresses/1.json
+      #
+      # == Failure or missing data
+      # A failure to find the requested object raises a ResourceNotFound
+      # exception if the find was called with an id.
+      # With any other scope, find returns nil when no data is returned.
+      #
+      #   Person.find(1)
+      #   # => raises ResourceNotFound
+      #
+      #   Person.find(:all)
+      #   Person.find(:first)
+      #   Person.find(:last)
+      #   # => nil
+      def find(*arguments)
+        scope   = arguments.slice!(0)
+        options = arguments.slice!(0) || {}
+        #Remove 'page' param
+        begin
+          options[:params].delete(:page)
+          options[:params].delete("page")
+        rescue
+        end
+
+        case scope
+        when :all
+          find_every(options)
+        when :first
+          collection = find_every(options)
+          collection && collection.first
+        when :last
+          collection = find_every(options)
+          collection && collection.last
+        when :one
+          find_one(options)
+        else
+          find_single(scope, options)
+        end
+      end
+
       def activate_session(session)
         raise InvalidSessionError.new("Session cannot be nil") if session.nil?
         self.site = session.site
